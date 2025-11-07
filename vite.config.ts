30cac8218772142435a770d873a2194a75c59f70
HaiTriu301 <haitriuwork30@gmail.com>
2025-11-08 00:14:57 +0700

    Final 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    build: {
-        outDir: "dist",
-    },
+    esbuild: {
+        jsxFactory: 'createElement',
+        jsxFragment: 'createFragment',
+    }
 });
